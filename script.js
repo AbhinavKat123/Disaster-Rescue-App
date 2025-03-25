5f3b9beefdbcc2edaae03ab38d09446dda213d2a
AbhinavKat123 <166365277+AbhinavKat123@users.noreply.github.com>
2025-03-25 18:22:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("helpMe").addEventListener("click", function() {
+    alert("Emergency help requested!");
+});
+
+document.getElementById("home").addEventListener("click", function() {
+    alert("Navigate to Home.");
+});
+
+document.getElementById("safety Tips").addEventListener("click", function() {
+    alert("Navigate to Safety Tips.");
+});
